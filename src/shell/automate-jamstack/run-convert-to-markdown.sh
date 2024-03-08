5b93bdeaf7a3b57e9f22044099dee88c27a206ec
Chinomso Ikwuagwu <chinomso.ikwuagwu@unite.eu>
2024-03-08 07:56:16 +0100

    Add blog post

--- src/shell/automate-jamstack/run-convert-to-markdown.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-printf "\nWorking from: %s" "$PWD"
-
-source /Users/chinomso/dev_chinomso/automate-jamstack/scripts/convert-to-markdown.sh \
-    -d '/Users/chinomso/dev_chinomso/automate-idea-to-social/src/python/main/resources/agent/pictory' \
-    -e docx
