82c82070cc1cd2e3dcf0561c4262a7d945d7bae5
Chinomso Ikwuagwu <chinomso.ikwuagwu@unite.eu>
2025-08-21 19:31:46 +0200

    Improve docker build and run process

--- shell/run.docker.sh
+++ shell/run.docker.sh
@@ -18,4 +18,22 @@ docker system prune -f
 # To log output of the build process.
 export BUILDKIT_PROGRESS=plain
 
-docker-compose -p automate-idea-to-social up -d
+docker-compose build aideas
+
+APP_USER_HOME="/root"
+APP_USER_ID=0
+
+# We don't want directories like OUTPUT_DIR, which our container
+# accesses to change (at least within the container), so we use a
+# fixed value for such directories. The value is fixed even though
+# the host directory they are bound to changes.
+docker-compose run -u "$APP_USER_ID" -p "${APP_PORT}:${APP_PORT}" \
+  -v /etc/localtime:/etc/localtime \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "${OUTPUT_DIR}:${APP_USER_HOME}/.aideas/output" \
+  -v "${CONTENT_DIR}:${APP_USER_HOME}/.aideas/content" \
+  -v "${CHROME_PROFILE_DIR}:${APP_USER_HOME}/.config/google-chrome" \
+  -e OUTPUT_DIR="${APP_USER_HOME}/.aideas/output" \
+  -e CONTENT_DIR="${APP_USER_HOME}/.aideas/content" \
+  -e CHROME_PROFILE_DIR="${APP_USER_HOME}/.config/google-chrome" \
+  aideas
