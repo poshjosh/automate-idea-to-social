92a07bc3c1e4dd1afdfe8e35b92c8fe0bb051bcd
Chinomso Ikwuagwu <chinomso.ikwuagwu@unite.eu>
2025-10-19 20:23:51 +0200

    Add context values to test-agent
    Add context values to test-agent and use test-agent
    for integration tests

--- src/test/app/agent_task_it.py
+++ src/test/app/agent_task_it.py
@@ -1,25 +1,26 @@
 import logging.config
 import unittest
 
+from aideas.app.config import RunArg
 from aideas.app.task import AgentTask
-from test_functions import get_main_config_loader, init_logging, get_run_context
-from agent.test_agent_factory import TestAgentFactory
+from test_functions import get_main_config_loader, init_logging
 
 init_logging(logging.config)
 
 
 class AgentTaskIT(unittest.TestCase):
     def test_run(self):
-        result = given_task().start()
-        print(f'{result.pretty_str()}')
+        result = given_task(["test-agent"]).start()
+        # print(f'{result.pretty_str()}')
         self.assertTrue(result.is_successful())
 
 
-def given_task() -> AgentTask:
-    # This will result to a live task, which we don't want
-    #return AgentTask.of_defaults(get_main_config_loader(), load_run_config())
-    agent_factory = TestAgentFactory(get_main_config_loader())
-    return AgentTask(agent_factory, get_run_context())
+def given_task(agent_names: list[str] = None) -> AgentTask:
+    config_loader = get_main_config_loader()
+    run_config = config_loader.load_run_config()
+    if agent_names:
+        run_config[str(RunArg.AGENTS.value)] = agent_names
+    return AgentTask.of_defaults(config_loader, run_config)
 
 
 if __name__ == '__main__':
